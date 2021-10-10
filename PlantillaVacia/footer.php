d21350f123422d16c51027bac9456b2cce66206d
William Lesle <lh15014@ues.edu.sv>
2021-10-09 21:58:51 -0600

    Separacion de partes.
    Se modifico adicion de header,footer y nav bar para re utilizar en otras paginas y se modifico el css de bs para el color de la pagina principal, lo demas esta igual.

---
+++ PlantillaVacia/footer.php
@@ -0,0 +1,17 @@
+<footer>
+                <div class="footer clearfix mb-0 text-muted">
+                    <div class="float-start">
+                        <p>2021 &copy; POLICIA NACIONAL CIVIL</p>
+                    </div>
+                    <div class="float-end">
+                        <p>Copyright <span class="text-danger"></span> by <a href="http://ahmadsaugi.com">UES-FMP</a></p>
+                    </div>
+                </div>
+            </footer>
+             <script src="assets/vendors/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+
+    <script src="assets/vendors/apexcharts/apexcharts.js"></script>
+    <script src="assets/js/pages/dashboard.js"></script>
+
+    <script src="assets/js/main.js"></script>
