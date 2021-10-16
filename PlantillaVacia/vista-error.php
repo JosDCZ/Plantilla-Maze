bbf1d001a04cd21b976b856db204a62de12a379d
Sary-osorio <or18018@ues.edu.sv>
2021-10-15 18:17:08 -0600

    active en menu y  vista error

--- PlantillaVacia/vista-error.php
+++
@@ -1,8 +0,0 @@
-<div class="container justify-content-center" style="width: 80%; height: 100%">
-    <img src="assets/images/logo/mantenimiento.png" class="card-img-top" alt="..." style="width: 35%; height: 35%">
-    <div class="card-body ">
-        <h1>En Mantenimiento</h1>
-
-
-    </div>
-</div>
